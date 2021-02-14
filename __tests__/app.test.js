6a0963395c4ed92d2484c58647d1bc96a97939ff
dl-watson <dlarkinwatson@gmail.com>
2021-02-13 20:07:10 -0800

    update misnamed import to connection/pool.js; updated README.md

---
+++ __tests__/app.test.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const pool = require("../lib/connection/pool");
+const request = require("supertest");
+const app = require("../lib/app");
+
+describe("bonus_full-stack-be routes", () => {
+  beforeEach(() => {
+    return pool.query(fs.readFileSync("./sql/setup.sql", "utf-8"));
+  });
+});
